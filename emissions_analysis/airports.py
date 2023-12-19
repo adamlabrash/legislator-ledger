3384035af0c6430292fd7ea9b7e6d654e96ac487
Adam <adamlabrash@protonmail.com>
2023-12-19 14:33:52 -0500

    further sanitize csv data

--- emissions_analysis/airports.py
+++ emissions_analysis/airports.py
@@ -1,13 +1,11 @@
 import csv
 import json
-from time import sleep
 from typing import Iterator
 
 from geopy.geocoders import Nominatim
 from geopy.location import Location
 from haversine import haversine
 from pydantic import BaseModel, model_validator
-from geopy.exc import GeocoderTimedOut
 
 '''
 This script extracts the unique locations from the travel expenditure reports 
@@ -96,11 +94,6 @@ def map_airport_data_to_travel_event_locations() -> Iterator[list[str]]:
 
 import csv
 
-'''
-Script that identifies missing location data
-TODO finish this
-'''
-
 
 def get_extracted_locations():
     locations = set()
@@ -111,95 +104,10 @@ def get_extracted_locations():
     return locations
 
 
-e_locs = extract_unique_locations_from_travel_events_csv()
-locs = get_extracted_locations()
+# e_locs = extract_unique_locations_from_travel_events_csv()
+# locs = get_extracted_locations()
 
-# TODO start here and manually add remaining locations
-for loc in e_locs:
-    if loc not in locs:
-        print(loc)
-
-import pdb
-
-pdb.set_trace()
-
-
-def get_missing_locations():
-    locations = get_extracted_locations()
-    with open('nextjs/data/travel_events_2.csv', 'r') as eventsfile:
-        eventsreader = csv.reader(eventsfile)
-        next(eventsreader)
-        missing_locations = set()
-        for row in eventsreader:
-            if row[-2] not in locations:
-                missing_locations.add(row[-2])
-            if row[-3] not in locations:
-                missing_locations.add(row[-3])
-        return missing_locations
-
-
-def get_location_data_from_travel_events_csv() -> Iterator[Location]:
-    geo_api = Nominatim(user_agent="GetLoc3")
-    airports = load_airport_data_from_json_file()
-    locations_new = []
-    locations = get_extracted_locations()
-    for location in get_missing_locations():
-        location = location.removesuffix(' (City / Ville)')
-        location = location.removesuffix(' (Town / Ville)')
-        location = location.split(' Indian Reserve')[0]
-        location = location.split('(')[0].strip()
-
-        if location in locations:
-            print(f"SKIP:{location}")
-            continue
-
-        if location == 'Montréal':
-            continue
-        try:
-            location_data = geo_api.geocode(location, country_codes='CA', timeout=10)
-            import pdb
-
-            pdb.set_trace()
-        except Exception:
-            print("TIMEOUT: ", location)
-            sleep(10)
-            location_data = geo_api.geocode(location, country_codes='CA')
-
-        if location_data:
-            closest_airport = min(
-                airports,
-                key=lambda airport: airport.distance_from_coordinates(location_data.latitude, location_data.longitude),
-            )
-            locations_new.append(
-                [
-                    location,
-                    location_data.latitude,
-                    location_data.longitude,
-                    closest_airport.iata_code,
-                    location_data.address,
-                ]
-            )
-            print("LOCATION FOUND: ", location)
-        else:
-            print("Location not found: ", location)
-
-    return locations_new
-
-
-locations_new = get_location_data_from_travel_events_csv()
-
-import pdb
-
-pdb.set_trace()
-
-
-try:
-    with open('emissions_analysis/locations.csv', 'a') as locationfile:
-        writer = csv.writer(locationfile)
-        for location_new in locations_new:
-            print(location_new)
-            writer.writerow(location_new)
-except:
-    import pdb
-
-    pdb.set_trace()
+# # TODO start here and manually add remaining locations
+# for loc in e_locs:
+#     if loc not in locs:
+#         print(loc)
