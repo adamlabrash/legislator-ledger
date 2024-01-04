419193748faaa866df23d2810890aff66f8405fc
Muhammad Haris <muhammadharis337@gmail.com>
2024-01-04 15:20:07 -0500

    Store Data in DataFrame
    Created Dataframe to store data from supabase and to make it easier to do operations on, and to use in the visualizations.

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,36 +1,23 @@
 import ECommerce from "@/components/Dashboard/E-commerce";
-import { createClient } from '@supabase/supabase-js'
 import Header from '@/components/Header'
 import { cookies } from 'next/headers'
+import { supabase } from '../services/supabaseClient'
+import {fetchTravelData} from '../services/fetchData'
+import {getCoords} from '../services/fetchData'
+import {fetchLocations} from '../services/fetchData'
+import TravelClaim from '../services/fetchData'
 
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { get } from "http";
 
 export default async function Index() {
-  const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-  const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
-  
-  if (!supabaseUrl || !supabaseAnonKey) {
-    throw new Error('Supabase URL and Anon Key must be provided!');
-  }
-  
-  const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
   //console.log(supabase)
-
-  async function fetchLocations() {
-    let { data: locations, error,count } = await supabase
-        .from('Locations')
-        .select('*', { count: 'exact' })
-    if (error) {
-        console.error('Error fetching locations:', error);
-        return;
-    }
-
-    console.log('Locations:', count );
-  }
-
-  fetchLocations();
+  try {
+    const data = await fetchTravelData();
+    let travelData: TravelClaim[] = data;
+    console.log(travelData[0]);
+} catch (error) {
+    console.error('Error fetching data:', error);
+}
 
   return (
     <>
@@ -38,3 +25,4 @@ export default async function Index() {
     </>
   )
 }
+ 
--- nextjs/components/Maps/MapOne.tsx
+++ nextjs/components/Maps/MapOne.tsx
@@ -1,9 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { VectorMap } from "@react-jvectormap/core";
 import { caMill  } from "@react-jvectormap/canada"; // This import depends on the actual name of the Canada map in the library
 
-const MapOne = () => {
+const MapOne= () => {
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-7">
       <h4 className="mb-2 text-xl font-semibold text-black dark:text-white">
