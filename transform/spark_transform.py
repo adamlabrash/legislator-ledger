40de132cb73e35a89ccc9c584164ac8e293797b1
Adam <adamlabrash@protonmail.com>
2024-01-09 16:08:05 -0500

    spark carbon analysis v2

--- transform/spark_transform.py
+++ transform/spark_transform.py
@@ -1,20 +1,22 @@
-from decimal import Decimal
+from enum import Enum
 import json
-from typing import Set
-from pydantic import BaseModel
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import upper
-import csv
-from shared_models.items import TravelEvent
 from transform.airport import determine_nearest_airport_to_location
 from transform.emissions_analysis.carbon_calculator.carbon_flight import CarbonFlight
 from haversine import haversine
 from geopy.geocoders import Nominatim
 from geopy.location import Location as GeoLocation
+from pyspark.sql.types import StringType, IntegerType, FloatType
+from pyspark.sql.functions import udf
+from pyspark.sql import DataFrame
 
-from emissions_analysis.carbon_calculator.carbon_flight import CarbonFlight
+
+class PassengerClass(Enum):
+    BUSINESS = 'business-class'
+    ECONOMY = 'economy-class'
 
 
+@udf(returnType=StringType())
 def normalize_location_str(
     location: str,
 ) -> str:  # for matching expenditure locations with existing locations in dataset
@@ -25,154 +27,162 @@ def normalize_location_str(
         .replace(' (Village)', '')
         .replace(' (District Municipality / Municipalité De District)', '')
         .replace('é', 'e')
+        .replace('Sainte', 'Saint')  # Replace ste with st? Saint with st?
         .upper()
         .strip()
     )
 
 
-class TravelLocation(BaseModel):
-    name: str
-    latitude: float
-    longitude: float
-    address: str
-    nearest_airport: str
-
-    @classmethod
-    def from_csv_row(cls, row: list) -> 'TravelLocation':
-        return cls(
-            name=row[0],
-            latitude=row[1],
-            longitude=row[2],
-            nearest_airport=row[3],
-            address=row[4],
-        )
-
-    def distance_from_coordinates(self, lat: float, lon: float) -> float:
-        return haversine((self.latitude, self.longitude), (lat, lon))
+@udf(returnType=IntegerType())
+def calc_carbon_emissions(departure_airport: str, destination_airport: str, passenger_class: PassengerClass) -> int:
+    return CarbonFlight().calculate_co2_from_airports(
+        departure_airport, destination_airport, passenger_class.value, trip_type='one-way'
+    )
 
 
-class Flight(TravelEvent):
-    departure: TravelLocation
-    destination: TravelLocation
-    cost: Decimal
-
-    @property
-    def est_carbon_footprint(self) -> int:
-        return CarbonFlight().calculate_co2_from_airports(
-            self.departure.nearest_airport, self.destination.nearest_airport, 'economy-class', trip_type='one-way'
-        )
+@udf(returnType=FloatType())
+def calc_distance_between_coordinates(
+    departure_lat: float, departure_lon: float, destination_lat: float, destination_lon: float
+) -> float:
+    # TODO fix this
+    return haversine((float(departure_lat), float(departure_lon)), (float(destination_lat), float(destination_lon)))
 
 
-def get_location_data_from_csv() -> dict[str, TravelLocation]:
-    locations: dict[str, TravelLocation] = {}
-
-    with open('transform/locations.csv') as f:
-        reader = csv.reader(f)
-        next(reader)
-        for row in reader:
-            location = TravelLocation.from_csv_row(row)
-            locations[normalize_location_str(location.name)] = location
-
-    return locations
-
-
-locations_dict = get_location_data_from_csv()
-
-
-# check if a direct match of normalized string is in locations.csv
-def get_direct_match_travel_location(location_str: str) -> TravelLocation | None:
-    normalized_location = normalize_location_str(location_str)
-    if normalized_location in locations_dict:
-        return locations_dict[normalized_location]
-
-    return None
-
-
-# check if a partial match of original string is in address fields in locations.csv
-def get_match_in_address_string(location_str: str) -> TravelLocation | None:
-    for _, location in locations_dict.items():
-        if location_str in location.address:
-            return location
-
-
-# def get_match_
-# for key, address in locations_dict.items():
-#     if key in location_str.split(' '):
-#         print(location)
-#         print(key)
-#         print(address)
-#         print()
-#         return Location.from_csv_row(locations_dict[key])
-
-new_locations: Set[TravelLocation] = set()
 geo_api = Nominatim(user_agent="GetLoc")
 
 
-def get_geo_api_location(location_str: str) -> TravelLocation | None:
-    geo_location: GeoLocation = geo_api.geocode(location_str, country_codes='CA', exactly_one=True)
+def get_geo_api_location_data(location_str: str) -> dict | None:
+    geo_location: GeoLocation = geo_api.geocode(location_str, country_codes='CA', exactly_one=True)  # type: ignore
     if geo_location is None:
         print("Unable to find location for:", location_str)
         return None
 
     closest_airport = determine_nearest_airport_to_location(geo_location.latitude, geo_location.longitude)
-    travel_location = TravelLocation(
-        name=location_str,
-        latitude=geo_location.latitude,
-        longitude=geo_location.longitude,
-        address=geo_location.address,
-        nearest_airport=closest_airport.iata_code,
-    )
-    new_locations.add(travel_location)
+    return {
+        'name': location_str,
+        'latitude': geo_location.latitude,
+        'longitude': geo_location.longitude,
+        'address': geo_location.address,
+        'nearest_airport': closest_airport.iata_code,
+    }
 
-
-# update locations.csv
-with open('transform/locations.csv', 'a') as f:
-    writer = csv.writer(f)
-    for new_location in new_locations:
-        writer.writerow(new_location.model_dump(mode='csv'))
-
-
-def determine_geo_data_of_location(location_str: str) -> TravelLocation | None:
-    return (
-        get_direct_match_travel_location(location_str)
-        or get_match_in_address_string(location_str)
-        or get_geo_api_location(location_str)
-    )
-
-
-with open('result.json', encoding='utf-8-sig') as f:
-    expenditure_data = json.load(f)
-
-flights: list[Flight] = []
-for expenditure in expenditure_data:
-    if expenditure['category'] == 'travel':
-        for event in expenditure['claim']['travel_events']:
-            if event['departure'] == '' or event['destination'] == '':
-                continue
-            flight_data = event | {
-                'departure': determine_geo_data_of_location(event['departure']),
-                'destination': determine_geo_data_of_location(event['destination']),
-                'cost': expenditure['claim']['travel_cost'],
-            }
-            flights.append(Flight.model_validate(flight_data))
-
-# write flight data to json
-with open('transform/flights.json', 'a') as f:
-    for flight in flights:
-        json.dump(flight.model_dump(mode='json'), f)  # TODO include carbon calculations
-
-
-'''
-#TODO fix .title() in expenditures dataset Mcmurray => McMurray
-#TODO implement spark
-'''
 
 spark = SparkSession.builder.getOrCreate()
 
-df = spark.read.json("result.json", multiLine=True)
-# df.printSchema()
-# df.show()
+
+def create_initial_dataframe() -> DataFrame:  # flatten travel expenditures from json
+    with open('expenditures.json', encoding='utf-8-sig') as f:
+        expenditure_data = json.load(f)
+
+    travel_expenditures: list[dict] = []
+    for expenditure in expenditure_data:
+        if expenditure['category'] == 'travel':
+            for event in expenditure['claim']['travel_events']:
+                travel_expenditures.append(expenditure | expenditure['claim'] | event)
+
+    # TODO make schema/struct or validate -> strings to ints etc
+    return spark.createDataFrame(data=travel_expenditures)
+
+
+def create_locations_dataframe() -> DataFrame:
+    return spark.read.csv('transform/locations.csv', header=True)
+
+
+# create initial travel dataframe and eliminate irrelevant columns
+travels_df = create_initial_dataframe()
+
+
+travels_df = travels_df.drop(
+    'claim', 'category', 'travel_events', 'accommodation_cost', 'meals_and_incidentals_cost', 'year', 'quarter'
+)
+travels_df.filter((travels_df.departure != '') | (travels_df.destination != ''))
+
+# normalize location strings
+travels_df = travels_df.withColumn('departure_normalized', normalize_location_str(travels_df.departure))
+travels_df = travels_df.withColumn('destination_normalized', normalize_location_str(travels_df.destination))
+
+locations_df = create_locations_dataframe()
+locations_df = locations_df.withColumn('location_normalized', normalize_location_str(locations_df.location))
+
+
+# map departure geolocation data
+travels_df = travels_df.join(
+    locations_df, travels_df.departure_normalized == locations_df.location_normalized, how='left_outer'
+)
+for location_col in locations_df.columns:
+    travels_df = travels_df.withColumnRenamed(location_col, f'departure_{location_col}')
+
+# map destination geolocation data
+travels_df = travels_df.join(
+    locations_df, travels_df.destination_normalized == locations_df.location_normalized, how='left_outer'
+)
+for location_col in locations_df.columns:
+    travels_df = travels_df.withColumnRenamed(location_col, f'destination_{location_col}')
+
+
+# TODO handle remaining null values -> get geopy data
+travels_df = travels_df.filter(
+    travels_df.destination_nearest_airport.isNotNull() & travels_df.departure_nearest_airport.isNotNull()
+)
+
+
+#### eliminate non-flight travel events
+
+# departure and destination cannot be the same
+travels_df.filter(travels_df.departure != travels_df.destination)
+
+flight_points_were_used = (
+    (travels_df.reg_points_used > 0) | (travels_df.special_points_used > 0) | (travels_df.USA_points_used > 0)
+)
+
+travels_df.filter(flight_points_were_used)
+# TODO look at other travel events in claim, determine if flight was used
+# is_flight_pattern = (travels_df.distance_travelled > 50) & (travels_df.transport_cost > 200)
+# price & distance, min distance of 50km, major airports (?)
+
+# calculate distance travelled
+travels_df = travels_df.withColumn(
+    'distance_travelled',
+    calc_distance_between_coordinates(
+        travels_df.departure_latitude,
+        travels_df.departure_longitude,
+        travels_df.destination_latitude,
+        travels_df.destination_longitude,
+    ),
+)
+
+# TODO estimate time of flight and calculate business/economy class
+
+
+travels_df = travels_df.withColumn(
+    'est_carbon_emissions',
+    calc_carbon_emissions(travels_df.departure_nearest_airport, travels_df.destination_nearest_airport),
+)
+
+
+from pyspark.sql.functions import year, col, sum, desc
+
+# query highest carbon emitters in 2022
+travels_df = travels_df.withColumn('year', year(travels_df.date))
+travels_df = travels_df.filter(travels_df.year == 2022)
+
+travels_df = travels_df.withColumn('est_carbon_emissions', travels_df['est_carbon_emissions'].cast(IntegerType()))
+
+result = (
+    travels_df.groupBy('name')
+    .agg(sum(col('est_carbon_emissions')).alias('total_carbon_emissions'))
+    .orderBy(desc('total_carbon_emissions'))
+    .limit(10)
+    .collect()
+)
+
+
 import pdb
 
 pdb.set_trace()
-df.withColumn('UPPER', upper(df.caucus)).show(n=5)
+# TODO dump values into json dataset & supabase csv files
+travels_df.show(n=1)
+
+'''
+#TODO fix .title() in expenditures dataset Mcmurray => McMurray
+'''
