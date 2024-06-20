3a9cd0bd5d908266331caec2885c092018ae2fbd
bob <adam.labrash@gmail.com>
2024-06-20 10:44:21 -0400

    Update documentation

--- src/analysis/dataframe_loader.py
+++ src/analysis/dataframe_loader.py
@@ -1,10 +1,4 @@
-import csv
-import json
-from typing import Any, Iterator
-
-from analysis.csv_loader import load_locations_csv, load_member_travel_claims_from_csv
-from analysis.items import Location
-from extraction.items import ExpenditureItem, MemberTravelClaim
+from analysis.base_data_loader import load_locations_csv, load_member_travel_claims_from_csv
 from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType
@@ -12,31 +6,18 @@ from pyspark.sql.types import StringType
 spark = SparkSession.builder.config("spark.driver.memory", "8g").getOrCreate()
 
 
-@udf(returnType=StringType())
-def normalize_location_str(location: str) -> str:
-    return (
-        location.replace('-', ' ')
-        .replace(' (Ville)', '')
-        .replace(' (City / Ville)', '')
-        .replace(' (Village)', '')
-        .replace(' (District Municipality / Municipalité De District)', '')
-        .replace('é', 'e')
-        .replace('Sainte', 'Saint')
-        .upper()
-        .strip()
-    )
+def initialize_locations_dataframe() -> DataFrame:
+    '''
+    Load locations.csv dataset into a spark dataframe
+    '''
+    locations_list = list(load_locations_csv())
+    locations_df = spark.createDataFrame(data=locations_list)
 
-
-def add_normalized_location_cols(travel_df: DataFrame) -> DataFrame:
-
-    # add normalized location columns
-    travel_df = travel_df.withColumn(
-        'departure_normalized', normalize_location_str(travel_df.departure)
+    # add normalized location column
+    locations_df = locations_df.withColumn(
+        'location_normalized', _normalize_location_str(locations_df.location)
     )
-    travel_df = travel_df.withColumn(
-        'destination_normalized', normalize_location_str(travel_df.destination)
-    )
-    return travel_df
+    return locations_df
 
 
 def initialize_travel_dataframe() -> DataFrame:
@@ -54,19 +35,30 @@ def initialize_travel_dataframe() -> DataFrame:
         'year',
         'quarter',
     )
-    travel_df = add_normalized_location_cols(travel_df)
+    travel_df = _add_normalized_location_cols(travel_df)
     return travel_df
 
 
-def initialize_locations_dataframe() -> DataFrame:
-    '''
-    Load locations.csv dataset into a spark dataframe
-    '''
-    locations_list = list(load_locations_csv())
-    locations_df = spark.createDataFrame(data=locations_list)
-
-    # add normalized location column
-    locations_df = locations_df.withColumn(
-        'location_normalized', normalize_location_str(locations_df.location)
+def _add_normalized_location_cols(travel_df: DataFrame) -> DataFrame:
+    travel_df = travel_df.withColumn(
+        'departure_normalized', _normalize_location_str(travel_df.departure)
     )
-    return locations_df
+    travel_df = travel_df.withColumn(
+        'destination_normalized', _normalize_location_str(travel_df.destination)
+    )
+    return travel_df
+
+
+@udf(returnType=StringType())
+def _normalize_location_str(location: str) -> str:
+    return (
+        location.replace('-', ' ')
+        .replace(' (Ville)', '')
+        .replace(' (City / Ville)', '')
+        .replace(' (Village)', '')
+        .replace(' (District Municipality / Municipalité De District)', '')
+        .replace('é', 'e')
+        .replace('Sainte', 'Saint')
+        .upper()
+        .strip()
+    )
